216e439063ccc929203e8c4a926de738b07ac762
bradjmarchant <bradjmarchant@gmail.com>
2020-10-31 11:41:24 -0700

    final

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,94 @@
-// @TODO: YOUR CODE HERE!
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Import Data
+d3.csv("assets/data/data.csv").then(function(healthData) {
+
+    // Step 1: Parse Data/Cast as numbers
+    // ==============================
+    healthData.forEach(function(data) {
+      data.smokes = +data.smokes;
+      data.age = +data.age;
+    });
+
+    // Step 2: Create scale functions
+    // ==============================
+    var xLinearScale = d3.scaleLinear()
+      .domain([d3.min(healthData, d => d.smokes)*.9, d3.max(healthData, d => d.smokes)*1.05])
+      .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+      .domain([d3.min(healthData, d => d.age)*.95, d3.max(healthData, d => d.age)*1.05])
+      .range([height, 0]);
+
+    // Step 3: Create axis functions
+    // ==============================
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // Step 4: Append Axes to the chart
+    // ==============================
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis);
+
+    chartGroup.append("g")
+      .call(leftAxis);
+
+    // Step 5: Create Circles
+    // ==============================
+    var circlesGroup = chartGroup.selectAll("circle")
+        .data(healthData);
+    var elements = circlesGroup
+        .enter();
+    var circle = elements
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.smokes))
+        .attr("cy", d => yLinearScale(d.age))
+        .attr("r", "15")
+        .classed("stateCircle",true)
+
+    var cirlceText = elements
+        .append("text")
+        .attr("x", d => xLinearScale(d.smokes))
+        .attr("y", d => yLinearScale(d.age))
+        .attr("dy",".35em")
+        .text(d => d.abbr)
+        .classed("stateText",true)
+
+
+
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 40)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Smoke");
+
+    chartGroup.append("text")
+      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("class", "axisText")
+      .text("Age");
+  }).catch(function(error) {
+    console.log(error);
+  });
